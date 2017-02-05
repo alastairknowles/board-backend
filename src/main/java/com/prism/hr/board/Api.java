4f76bb391077c99732f375dc213fd5996cb5ec71
alastairknowles <alastair@prism.hr>
2017-02-05 18:58:34 +0000

    Adjustment to accept Stormpath requests from the client.

--- src/main/java/com/prism/hr/board/Api.java
+++ src/main/java/com/prism/hr/board/Api.java
@@ -7,19 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class Api {
     
-    private AccountResolver accountResolver = AccountResolver.INSTANCE;
-    
-    @RequestMapping(value = "/api", method = RequestMethod.GET)
-    public String api(HttpServletRequest request) {
-        Account account = accountResolver.getAccount(request);
-        if (account == null) {
-            return "redirect:/login?next=api";
-        }
-        
-        return "api";
+    @RequestMapping(value = {"/register", "/login", "/forgot"}, method = RequestMethod.GET, headers = "Accept=text/html")
+    public String authentication(HttpServletResponse response) {
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setHeader("Pragma", "no-cache");
+        return "forward:/";
     }
     
 }

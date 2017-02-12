ab32cb6f52003f93a78b0226ae79d0e709503ee1
alastairknowles <alastair@prism.hr>
2017-02-12 18:22:08 +0000

    Clean up.

--- src/main/java/com/prism/hr/board/Api.java
+++ src/main/java/com/prism/hr/board/Api.java
@@ -1,20 +1,17 @@
 package com.prism.hr.board;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.servlet.http.HttpServletResponse;
-
 @Controller
 public class Api {
     
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @RequestMapping(value = {"/register", "/login", "/forgot", "/logout"}, method = RequestMethod.GET)
-    public void suppressStormpathMvcViews(HttpServletResponse response) {
+    public void suppressStormpathMvcViews() {
     }
     
 }

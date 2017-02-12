f43f23adef01d245fecb11e22692839b1e77c3b6
alastairknowles <alastair@prism.hr>
2017-02-12 02:07:36 +0000

    First test passing, we can create an account and get an authentication token for it using username password authentication, so now possible to test business logic like creating department, board etc.

--- src/main/java/com/prism/hr/board/Api.java
+++ src/main/java/com/prism/hr/board/Api.java
@@ -1,22 +1,19 @@
 package com.prism.hr.board;
 
-import com.stormpath.sdk.account.Account;
-import com.stormpath.sdk.servlet.account.AccountResolver;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class Api {
     
-    @RequestMapping(value = {"/register", "/login", "/forgot"}, method = RequestMethod.GET, headers = "Accept=text/html")
-    public String authentication(HttpServletResponse response) {
-        response.setHeader("Cache-Control", "no-store, no-cache");
-        response.setHeader("Pragma", "no-cache");
-        return "forward:/";
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @RequestMapping(value = {"/register", "/login", "/forgot"}, method = RequestMethod.GET)
+    public void suppressStormpathMvcViews(HttpServletResponse response) {
     }
     
 }
--- src/test/java/com/prism/hr/board/ApiTest.java
+++ src/test/java/com/prism/hr/board/ApiTest.java
@@ -1,53 +1,37 @@
 package com.prism.hr.board;
 
-import com.prism.hr.board.rest.dto.RegistrationDTO;
-import com.prism.hr.board.util.RegistrationUtil;
+import com.prism.hr.board.object.AccountPassword;
+import com.prism.hr.board.service.AccountTestService;
+import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.oauth.OAuthGrantRequestAuthenticationResult;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.Map;
 
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {ApplicationConfiguration.class, ApplicationTestConfiguration.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {ApplicationConfiguration.class})
 public class ApiTest {
     
     @Autowired
-    private MockMvc mockMvc;
-    
-    @Autowired
-    private ApplicationTestConfiguration applicationTestConfiguration;
+    private AccountTestService accountTestService;
     
     @Test
-    public void shouldAcceptUserRegistration() throws Exception {
-        RegistrationDTO registrationDTO = RegistrationUtil.getRegistrationDTO();
+    public void shouldCreateAndAuthenticateUser() throws Exception {
+        AccountPassword accountPassword = accountTestService.createTestAccount();
+        Account account = accountPassword.getAccount();
         
-        String response = mockMvc.perform(MockMvcRequestBuilders.post("/register")
-                .param("givenName", registrationDTO.getGivenName())
-                .param("surname", registrationDTO.getSurname())
-                .param("email", registrationDTO.getEmail())
-                .param("password", registrationDTO.getPassword())
-                .accept(MediaType.TEXT_HTML)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString();
+        OAuthGrantRequestAuthenticationResult authenticationResult = accountTestService.authenticateTestAccount(account.getEmail(), accountPassword.getPassword());
+        Assert.assertNotNull(authenticationResult.getAccessToken());
+        Assert.assertNotNull(authenticationResult.getRefreshToken());
         
-        Map<String, RegistrationDTO> userCache = applicationTestConfiguration.getUserCache();
-        Assert.assertEquals(1, userCache.size());
-        
-        RegistrationDTO registrationDTOProcessed = userCache.get(registrationDTO.getEmail());
-        Assert.assertNotNull(registrationDTOProcessed);
+        account.delete();
     }
     
 }

09dd0e9432420cc660439a650850f6b0a0ac2b80
fibinger <kuba@fibinger.pl>
2017-02-16 19:48:15 +0100

    Minor logout fix, context path specified, .gitignore adjusted

--- src/main/java/com/prism/hr/board/Api.java
+++ src/main/java/com/prism/hr/board/Api.java
@@ -1,17 +1,21 @@
 package com.prism.hr.board;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class Api {
-    
+
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @RequestMapping(value = {"/register", "/login", "/forgot", "/logout"}, method = RequestMethod.GET)
     public void suppressStormpathMvcViews() {
     }
-    
+
+    @RequestMapping(value = "/postLogout", method = RequestMethod.POST)
+    public void postLogout() {
+    }
+
 }
